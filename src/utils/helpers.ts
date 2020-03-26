76752a2b0e35967e4f489ac770a6cc85031b9d81
Jakob Árni H. Ísleifsson <jakobar@jakobar.is>
2020-03-26 22:41:34 +0000

    Fix removal of zeros at end of decimals

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,7 @@
 export function formatNumber(numb: number, decimals: number = 5) {
-  return numb.toFixed(decimals).replace(/[.,]00$/, '');
+  if ( numb % 1 !== 0) {
+    return numb.toFixed(decimals).replace(/0+$/, '');
+  } else {
+    return numb;
+  }
 }
