9390f69369ad22e415d3bb2ff7ce56e712ee6013
Jakob Árni H. Ísleifsson <jakobar@jakobar.is>
2020-04-09 11:31:10 +0000

    Add formulas

--- src/formulas.js
+++ src/formulas.js
@@ -52,6 +52,27 @@ export default {
           }, {
             id: 'gram',
             formula: 'x * 1e+6',
+          }, {
+            id: 'milligram',
+            formula: 'x * 1e+9',
+          }, {
+            id: 'microgram',
+            formula: 'x * 1e+12',
+          }, {
+            id: 'imperialton',
+            formula: 'x / 1.016',
+          }, {
+            id: 'uston',
+            formula: 'x / 1.102',
+          }, {
+            id: 'stone',
+            formula: 'x * 157',
+          }, {
+            id: 'pund',
+            formula: 'x * 2205',
+          }, {
+            id: 'ounce',
+            formula: 'x * 35274',
           }],
         }, {
           id: 'kilogram',
@@ -63,6 +84,27 @@ export default {
           }, {
             id: 'gram',
             formula: 'x * 1000',
+          }, {
+            id: 'milligram',
+            formula: 'x * 1e+6',
+          }, {
+            id: 'microgram',
+            formula: 'x * 1e+9',
+          }, {
+            id: 'imperialton',
+            formula: 'x / 1016',
+          }, {
+            id: 'uston',
+            formula: 'x / 907',
+          }, {
+            id: 'stone',
+            formula: 'x / 6.35',
+          }, {
+            id: 'pund',
+            formula: 'x * 2.205',
+          }, {
+            id: 'ounce',
+            formula: 'x * 35.274',
           }],
         }, {
           id: 'gram',
@@ -74,6 +116,60 @@ export default {
           }, {
             id: 'kilogram',
             formula: 'x / 1000',
+          }, {
+            id: 'milligram',
+            formula: 'x * 1000',
+          }, {
+            id: 'microgram',
+            formula: 'x * 1e+6',
+          }, {
+            id: 'imperialton',
+            formula: 'x / 1.016e+6',
+          }, {
+            id: 'uston',
+            formula: 'x / 907185',
+          }, {
+            id: 'stone',
+            formula: 'x / 6350',
+          }, {
+            id: 'pund',
+            formula: 'x / 454',
+          }, {
+            id: 'ounce',
+            formula: 'x / 28.35',
+          }],
+        },
+        , {
+          id: 'milligram',
+          name: 'Milligram',
+          code: 'mg',
+          formulas: [{
+            id: 'tonne',
+            formula: 'x / 1e+9',
+          }, {
+            id: 'kilogram',
+            formula: 'x / 1e+6',
+          }, {
+            id: 'gram',
+            formula: 'x / 1000',
+          }, {
+            id: 'microgram',
+            formula: 'x * 1000',
+          }, {
+            id: 'imperialton',
+            formula: 'x / 1.016e+9',
+          }, {
+            id: 'uston',
+            formula: 'x / 9.072e+8',
+          }, {
+            id: 'stone',
+            formula: 'x / 6.35e+6',
+          }, {
+            id: 'pund',
+            formula: 'x / 453592',
+          }, {
+            id: 'ounce',
+            formula: 'x / 28350',
           }],
         },
       ],
