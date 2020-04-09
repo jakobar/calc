6f0bc03d0dcf08b6be8df28dd5ebec01de50c5c1
Jakob Árni H. Ísleifsson <jakobar@jakobar.is>
2020-04-09 11:31:18 +0000

    Change formatting

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,3 @@
-export function formatNumber(numb: number, decimals: number = 5) {
-  if ( numb % 1 !== 0) {
-    return numb.toFixed(decimals).replace(/0+$/, '');
-  } else {
-    return numb;
-  }
+export function formatNumber(numb: number) {
+  return numb.toPrecision();
 }
